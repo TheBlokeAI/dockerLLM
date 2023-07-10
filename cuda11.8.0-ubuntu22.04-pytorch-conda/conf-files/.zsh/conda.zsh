cd72c840ad488e102fe0718f610bcd36ba5e3255
TheBloke <tomjobbins@gmail.com>
2023-07-10 11:40:14 +0000

    Further WIP changes to pytorch-conda and train. Not yet ready for release, should be in a branch really but eh

--- cuda11.8.0-ubuntu22.04-pytorch-conda/conf-files/.zsh/conda.zsh
+++ cuda11.8.0-ubuntu22.04-pytorch-conda/conf-files/.zsh/conda.zsh
@@ -1,6 +1,14 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
+
+# Default conda home is in the container
 CONDA="$HOME/miniconda3"
+
+# But if there's one on the volume, use that instead
+if [[ -d /workspace/miniconda3 ]]; then
+  CONDA=/workspace/miniconda3
+fi
+
 __conda_setup="$('$CONDA/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
--- cuda11.8.0-ubuntu22.04-pytorch-conda/start-ssh-only.sh
+++ cuda11.8.0-ubuntu22.04-pytorch-conda/start-ssh-only.sh
@@ -7,7 +7,7 @@ if [[ $PUBLIC_KEY ]]; then
 	mkdir -p ~/.ssh
 	chmod 700 ~/.ssh
 	cd ~/.ssh
-	echo $PUBLIC_KEY >>authorized_keys
+	echo "${PUBLIC_KEY}" >>authorized_keys
 	chmod 700 -R ~/.ssh
 	cd /
 	service ssh start
